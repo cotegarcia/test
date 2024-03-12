1af3f314aed928e1b70c4361d77444ff16849c01
Juan Jose Garcia <juanjosgarciachau@gmail.com>
2024-03-12 12:10:08 -0500

    Base de datos mensaje

--- project_task.js
+++ project_task.js
@@ -16,8 +16,32 @@ const generarMsj = () => {
     const animal = seleccLista(animales)
     const comida = seleccLista(comidas)
     const lugar = seleccLista(lugares)
-    return `El ${animal} quiere comer ${comida} en ${lugar}`
+    const x = `El ${animal} quiere comer ${comida} en ${lugar}`
+    return x   
 }
 
-console.log(generarMsj)
+// funcion para guardar mensaje 
+
+const fs = require('fs');
+
+const keepMessage = mensaje => {
+    const rutaArchivo = "/g/Mi unidad/05_workstation/01_works/04_codigo/workspace/04_estudios/02_libros_y_cursos/ca_full-stack/javascript/mensajes.txt"
+    fs.appendFile(rutaArchivo, mensaje + '\n', (err) => {
+        if (err) throw err;
+        console.log('Mensaje guardado')
+    });
+
+}
+
+
+const mensaje = generarMsj();
+console.log(mensaje);
+keepMessage(mensaje);
+
+
+
+
+
+
+
 
